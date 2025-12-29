704d2d2b4b4dad7c928197b41e2069e58745a281
sh-qadri <shujauddin@euphotic.io>
2025-12-30 00:30:00 +0530

    Enabled edge to edge

--- android-app/app/src/main/java/com/qadri/to_do/MainActivity.kt
+++ android-app/app/src/main/java/com/qadri/to_do/MainActivity.kt
@@ -3,13 +3,11 @@ package com.qadri.to_do
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.qadri.to_do.ui.ToDoApp
 import com.qadri.to_do.ui.theme.ToDoTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,6 +16,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             ToDoTheme {
                 // A surface container using the 'background' color from the theme
