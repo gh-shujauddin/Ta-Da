b4fcb11ac96533eee019269d5d1d3e5a480af164
sh-qadri <shujauddin@euphotic.io>
2025-12-31 00:19:11 +0530

    DTO for login and signup requests

---
+++ tada/src/main/java/com/qadri/tada/dto/LoginResponseDto.java
@@ -0,0 +1,15 @@
+package com.qadri.tada.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponseDto {
+    private Long userId;
+    private String token;
+}
